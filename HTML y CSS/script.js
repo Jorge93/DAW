84438cd9883dd1b1f668fb6974e1f4fb98cc2caf
Jorge93 <93jorge93@gmail.com>
2015-02-15 13:52:07 +0100

    jQuery perfeccionado

--- HTML y CSS/script.js
+++ HTML y CSS/script.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
     $("#menu-horizontal ul").hover(function () {
-        $(this).find("ul>li").fadeToggle(500);
+        $(this).find("ul>li").fadeIn({duration: 500, queue:false }).slideDown(800);
+    }, function(){
+        $(this).find("ul>li").fadeOut({ duration: 500, queue: false }).slideUp(800);
     });
 });
+
+
+
+
+
+
+
+
+
+
+
